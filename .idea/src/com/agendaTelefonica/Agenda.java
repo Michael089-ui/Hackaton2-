8d33e3059abd43bde8dc8dc232f895026533bf6d
Michael Estiven Corrales Mendez <corralesmendezm@gmail.com>
2025-05-16 10:16:19 -0500

    Primera estructra de carpetas para trabajar en clases Agenda y Contacto

---
+++ .idea/src/com/agendaTelefonica/Agenda.java
@@ -0,0 +1,4 @@
+package com.agendaTelefonica;
+
+public class Agenda {
+}
---
+++ .idea/src/com/agendaTelefonica/Main.java
@@ -0,0 +1,7 @@
+package com.agendaTelefonica;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
